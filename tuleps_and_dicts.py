82e035084a18db4a5ef3b1aed4ad8aae91738ecc
Milan Griffin <milanagriffin4@gmail.com>
2022-07-21 09:34:00 -0500

    message

---
+++ tuleps_and_dicts.py
@@ -0,0 +1,100 @@
+'''
+def sum_tuple(tuple):
+    total=0
+        for in range(len(tuple)):
+            total = total + tuple [i]
+            return total
+
+'''
+
+
+
+'''
+tup = 10,8,23,11
+print sum(tuple)
+'''
+
+
+
+'''
+
+def first_two(tuple):
+    return (tuple[0],tuple[1])
+
+print (first_two(("yes","ok")))
+    returns("yes","ok")
+'''
+'''
+def add_tuples(tuple1, tuple2):
+    t= tuple1[0] + tuple2[0]
+    tupe= tuple1[1] + tuple2[1]
+    return (t, tupe)
+ 
+
+print (add_tuples((5,10),(15,20)))
+print (add_tuples((50,100),(150,200)))
+print (add_tuples((60,160),(70,170)))
+print (add_tuples((6,16),(7,17)))
+print (add_tuples((100,10),(200,20)))
+print (add_tuples((10,10),(20,20)))
+
+
+
+
+
+
+
+
+def print_dict(dict):
+    for key in dict:
+        print(dict[key])
+        
+
+
+        
+
+
+
+
+Milana07
+def list_to_dict(1st1, lst2):
+    dict={}
+    for dict in range(len(Lines)):
+        l1 = lst1[i]
+        l2 = lst2[i]
+        dict[key]=value
+    return dict
+
+
+
+
+
+
+
+
+
+'''
+
+
+
+'''
+print("<!DOCTYPE html>")
+print("<htm1>")
+
+print("<head>")
+print("<style>")
+print("  hr {margin-top:0; margin-bottom:0}")
+print("</style>")
+print("</head>")
+
+print("<body>")
+for color in range (255):
+    print(f'<hr style = "color:rgb({color},{color},{color})">')
+print("</body>")
+
+print("</html>")
+'''
+
+
+def calculate_total(prices)
+prices_list = [7.4, 5, 4.40, 3.64, 30, 90.07, 7.00, 1.10, 64, 6.25, 14.26, 11.05, 7.57, 10.36, 13.15, 11.06, 3.74, 15.12, 19.32, 14.73].
